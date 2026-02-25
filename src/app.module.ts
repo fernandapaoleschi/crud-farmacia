0880bca5bd5656dcf098dbfd572f74d0a55ee17a
Fernanda Paoleschi <fernandapaoleschi1@gmail.com>
2026-02-25 13:20:38 -0300

    bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+
 import { ProdService } from './data/prod.service';
+
 import { AppController } from './app.controller';
+
+import { CategoriaModule } from './categoria/categoria.module';
+import { ProdutoModule } from './produto/produto.module';
+
 @Module({
   imports: [
-  ConfigModule.forRoot(),
-  TypeOrmModule.forRootAsync({
-	useClass: ProdService,
-    imports: [ConfigModule],
-}),
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
+    }),
+
+    CategoriaModule,
+    ProdutoModule,
   ],
   controllers: [AppController],
   providers: [],
